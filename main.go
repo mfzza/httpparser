0372d17d34aa11c0f25d7aa5292b5f587f122461
fatkhulizz <fatkhul7izza@gmail.com>
2025-10-27 12:27:01 +0700

    handle multiple value in header field

--- main.go
+++ main.go
@@ -6,33 +6,53 @@ import (
 	"strings"
 )
 
+// map with key: string, value: slice of string
+type Header map[string][]string
+
 type httpParser struct {
 	startLine string
-	header    map[string]string
-	headerKey []string // to know it order
+	header    Header
+	headerKey []string //NOTE: to know it order, maybe not really needed?
 	body      string
 }
 
 func newHttpParser() *httpParser {
-	return &httpParser{header: make(map[string]string)}
+	return &httpParser{header: make(map[string][]string)}
 }
 
 func (h *httpParser) parseHead(head string) {
 	for field := range strings.SplitSeq(head, "\n") {
 		if strings.Contains(field, ":") {
-			splits := strings.SplitN(field, ":", 2)
-			key := strings.TrimSpace(splits[0])
-			val := strings.TrimSpace(splits[1])
+			parts := strings.SplitN(field, ":", 2)
+			key := strings.TrimSpace(parts[0])
 			h.headerKey = append(h.headerKey, key)
-			h.header[key] = val
+
+			valparts := strings.TrimSpace(parts[1])
+
+			for val := range strings.SplitSeq(valparts, ",") {
+				h.header[key] = append(h.header[key], val)
+			}
+
 		}
 	}
 
 }
 
 func (h *httpParser) printHead() {
-	for _, k := range h.headerKey {
-		fmt.Println("["+k+"]:", h.header[k])
+	for _, key := range h.headerKey {
+		fmt.Print(" - ", key, ": ")
+
+		// print inline if only one
+		if len(h.header[key]) <= 1 {
+			fmt.Println(h.header[key][0])
+
+		// print multiple line if more
+		} else {
+			fmt.Println()
+			for _, val := range h.header[key] {
+				fmt.Println("\t-", val)
+			}
+		}
 	}
 }
 
@@ -44,6 +64,7 @@ Connection: keep-alive
 Content-Length: 1630
 Cache-Control: max-age=0
 Upgrade-Insecure-Requests: 1
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
 Origin: http://192.168.100.109`
 
 	h := newHttpParser()
