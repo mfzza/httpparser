60488909f3eb62f2cafdf1991c8bc9b4fe62abbd
fatkhulizz <fatkhul7izza@gmail.com>
2025-10-31 14:03:32 +0700

    revert to handle only one argument

--- main.go
+++ main.go
@@ -8,31 +8,33 @@ import (
 )
 
 func main() {
+	warning := func() string {
+		return "USAGE\t: go run . <path-to-file>\nEXAMPLE\t: go run . test/testdata/multipart.txt"
+	}
 	if len(os.Args) < 2 {
-		fmt.Fprintln(os.Stderr, "USAGE\t: go run . <path-to-file>")
-		fmt.Println("EXAMPLE\t: go run . test/testdata/multipart.txt")
+		fmt.Fprintln(os.Stderr, warning())
+		os.Exit(2)
+	}
+
+	if len(os.Args) > 2 {
+		fmt.Fprintln(os.Stderr, "Too many arguments\n" + warning())
+		os.Exit(2)
+	}
+
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error opening a file: %v\n", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	r := bufio.NewReader(file)
+	hp, err := httpParser.NewHttpParser(r)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error parsing HTTP Message: %v\n", err)
 		os.Exit(1)
 	}
 
-	for _, filepath := range os.Args[1:] {
-		// file, err := os.Open(os.Args[1])
-		file, err := os.Open(filepath)
-		fmt.Println("****************************************************************")
-		fmt.Println("FILE:", filepath)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error opening a file: %v\n", err)
-			continue
-		}
-		defer file.Close()
-
-		r := bufio.NewReader(file)
-		hp, err := httpParser.NewHttpParser(r)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error parsing HTTP Message: %v\n", err)
-			continue
-		}
-
-		hp.Print()
-	}
-
+	fmt.Println("FILE:", os.Args[1] )
+	hp.Print()
 }
